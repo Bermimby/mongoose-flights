19a4a631f1f663dc831bbaf82c082c1b83b98e5a
Bermimby <bermaimbert@hotmail.com>
2023-01-18 21:01:54 -0500

    First Commit

---
+++ controllers/flights.js
@@ -0,0 +1,14 @@
+const Flight = require('../models/flight')
+
+
+module.exports = {
+    index,
+}
+
+function index (req, res) {
+    Flight.find({}, function(err, movies) {
+      res.render('flights/index', { title: 'All Flights', flights });
+    });
+  }
+
+  
